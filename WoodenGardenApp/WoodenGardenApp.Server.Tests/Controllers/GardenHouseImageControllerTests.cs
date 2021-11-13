550330eb464219a62c9f1117e0f54874c4318e7c
Noweli <patryklewonn@gmail.com>
2021-11-13 21:15:08 +0100

    GardenHouseImageControllerTests.cs template

---
+++ WoodenGardenApp/WoodenGardenApp.Server.Tests/Controllers/GardenHouseImageControllerTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using NUnit.Framework;
+using WoodenGardenApp.Server.Controllers;
+using WoodenGardenApp.Server.Data;
+
+namespace ServerUnitTests.Controllers;
+
+[TestFixture]
+public class GardenHouseImageControllerTests
+{
+    private GardenHouseController? _gardenHouseController;
+
+    [SetUp]
+    public void SetUp()
+    {
+        var options = new DbContextOptionsBuilder().Options;
+
+        var appDbContextMock = new Mock<ApplicationDbContext>(options, null!);
+        _gardenHouseController = new GardenHouseController(appDbContextMock.Object);
+    }
+    
+    
+}
