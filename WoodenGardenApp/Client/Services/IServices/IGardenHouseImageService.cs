b228108e4099ba0f48e77dfa3519bec4a5d8d37b
Noweli <patryklewonn@gmail.com>
2021-11-17 18:15:08 +0100

    GardenHosueImage service interface

---
+++ WoodenGardenApp/Client/Services/IServices/IGardenHouseImageService.cs
@@ -0,0 +1,7 @@
+﻿namespace WoodenGardenApp.Client.Services.IServices;
+
+public interface IGardenHouseImageService
+{
+    Task AddImage(int? roomId, string imageUrl);
+    Task DeleteImage(int? imageId);
+}
