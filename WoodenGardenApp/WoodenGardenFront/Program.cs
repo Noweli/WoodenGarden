5aa1c33e7f1ac5ba8b6c8f26a966b9185522d8dc
Noweli <patryklewonn@gmail.com>
2021-12-12 21:53:32 +0100

    Added options to add new garden house

--- WoodenGardenApp/WoodenGardenFront/Program.cs
+++ WoodenGardenApp/WoodenGardenFront/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using WoodenGardenFront;
+using WoodenGardenFront.Services;
+using WoodenGardenFront.Services.IServices;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
+builder.Services.AddScoped<IGardenHouseService, GardenHouseService>();
 
 await builder.Build().RunAsync();
