5501b1c919226fb995f8ace4b77221876744ad79
Noweli <patryklewonn@gmail.com>
2021-12-18 21:37:19 +0100

    Remove file upload. It will be handled by database, where images will be stored as Base64

--- WoodenGardenApp/WoodenGardenFront/Services/IServices/IFileUpload.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-
-namespace WoodenGardenFront.Services.IServices;
-
-public interface IFileUpload
-{
-    Task<string> UploadFile(IBrowserFile? file);
-    Task<bool> DeleteFile(string fileName);
-}
