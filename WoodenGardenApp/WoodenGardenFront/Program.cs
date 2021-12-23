6e5f3863e7df359bd08ae56618a9cf2f6964f25c
Noweli <patryklewonn@gmail.com>
2021-12-23 21:53:31 +0100

    Added garden house image handling on front end

--- WoodenGardenApp/WoodenGardenFront/Program.cs
+++ WoodenGardenApp/WoodenGardenFront/Program.cs
@@ -11,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient {BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)});
 builder.Services.AddScoped<IGardenHouseService, GardenHouseService>();
 builder.Services.AddScoped<IGardenHouseImageService, GardenHouseImageService>();
+builder.Services.AddScoped<IImageHandlingService, ImageHandlingService>();
 
 await builder.Build().RunAsync();
