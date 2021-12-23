0209abe55bb24fe0219b792afe6b0cf61e69eac9
Noweli <patryklewonn@gmail.com>
2021-12-24 00:13:04 +0100

    Removed front webassembly project

--- WoodenGardenApp/WoodenGardenFront/Services/IServices/IGardenHouseImageService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace WoodenGardenFront.Services.IServices;
-
-public interface IGardenHouseImageService
-{
-    Task AddImage(int roomId, string imageBase64);
-    Task DeleteImage(int imageId);
-}
--- WoodenGardenApp/WoodenGardenFront/Services/IServices/IImageHandlingService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
-
-namespace WoodenGardenFront.Services.IServices;
-
-public interface IImageHandlingService
-{
-    Task<string> SaveImage(IBrowserFile image);
-    Task<bool> DeleteImage(string imagePath);
-}
