0ec25a9a94c828ed35e1b38b61a0c0d8d3909131
Noweli <patryklewonn@gmail.com>
2021-12-23 21:28:48 +0100

    Create image handling service interface and inheriting class

---
+++ WoodenGardenApp/WoodenGardenFront/Services/IServices/IImageHandlingService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace WoodenGardenFront.Services.IServices;
+
+public interface IImageHandlingService
+{
+    Task<string> SaveImage(IBrowserFile image);
+    bool DeleteImage(string imagePath);
+}
---
+++ WoodenGardenApp/WoodenGardenFront/Services/ImageHandlingService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using WoodenGardenFront.Services.IServices;
+
+namespace WoodenGardenFront.Services;
+
+public class ImageHandlingService : IImageHandlingService
+{
+    public Task<string> SaveImage(IBrowserFile image)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool DeleteImage(string imagePath)
+    {
+        throw new NotImplementedException();
+    }
+}
