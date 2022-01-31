61ebd4e7bb7281ea1bf16020240260ae9884b814
Noweli <patryklewonn@gmail.com>
2022-01-31 19:33:06 +0100

    Order page

--- WoodenGardenApp/Shared/Models/Database/Order.cs
+++ WoodenGardenApp/Shared/Models/Database/Order.cs
@@ -1,10 +1,17 @@
-﻿namespace WoodenGardenApp.Shared.Models.Database;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WoodenGardenApp.Shared.Models.Database;
 
 public class Order
 {
     public int Id { get; set; }
+    [Required]
     public string? RequesterName { get; set; }
+    [Required]
+    [EmailAddress]
     public string? RequesterEmail { get; set; }
+    [Phone]
     public string? RequesterPhoneNo { get; set; }
+    [Required]
     public string? Description { get; set; }
 }
