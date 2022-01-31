45957e2e77adad12033f0ab117c40ae18f79f75b
Noweli <patryklewonn@gmail.com>
2022-01-31 19:45:06 +0100

    Error messages

--- WoodenGardenApp/Shared/Models/Database/Order.cs
+++ WoodenGardenApp/Shared/Models/Database/Order.cs
@@ -5,13 +5,13 @@ namespace WoodenGardenApp.Shared.Models.Database;
 public class Order
 {
     public int Id { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Name is required.")]
     public string? RequesterName { get; set; }
-    [Required]
-    [EmailAddress]
+    [Required(ErrorMessage = "Email is required.")]
+    [EmailAddress(ErrorMessage = "Email is required and needs to be valid email.")]
     public string? RequesterEmail { get; set; }
-    [Phone]
+    [Phone(ErrorMessage = "Phone number, if entered, needs to be valid phone number.")]
     public string? RequesterPhoneNo { get; set; }
-    [Required]
+    [Required(ErrorMessage = "Order description is required.")]
     public string? Description { get; set; }
 }
